49ab5f0b4aac28958ce9a6e9c095ed3868c8c6b0
Pixiepinks <info@pixiepinks.shop>
2025-11-23 10:22:40 +0530

    Add CORS support for frontend access

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class BaseConfig:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///local.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "change-this-secret")
+    CORS_ORIGINS = os.getenv("CORS_ORIGINS", "*")
 
 
 class DevelopmentConfig(BaseConfig):
