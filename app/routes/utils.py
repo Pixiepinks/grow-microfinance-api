17797d73cdfaf359d4d775b5f69b7726c867c026
Pixiepinks <info@pixiepinks.shop>
2025-12-04 14:51:11 +0530

    Allow CORS preflight on loan application listings

--- app/routes/utils.py
+++ app/routes/utils.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from flask import jsonify
+from flask import jsonify, request, current_app
 from flask_jwt_extended import verify_jwt_in_request, get_jwt
 
 
@@ -7,6 +7,12 @@ def role_required(roles):
     def decorator(fn):
         @wraps(fn)
         def wrapper(*args, **kwargs):
+            if request.method == "OPTIONS":
+                current_app.logger.debug(
+                    "CORS preflight for %s %s", request.method, request.path
+                )
+                return current_app.make_default_options_response()
+
             verify_jwt_in_request()
             claims = get_jwt()
             if claims.get("role") not in roles:
